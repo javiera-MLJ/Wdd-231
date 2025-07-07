b7cffde8a99b39f0f4b9e857d0db6e988fb1d680
javiera-MLJ <lor24003@byui.edu>
2025-07-07 19:57:15 -0400

    activity first week

---
+++ scripts/navigation.js
@@ -0,0 +1,7 @@
+const navButton = document.querySelector('#nav-button');
+const navlinks = document.querySelector('#nav-bar');
+
+navButton.addEventListener('click', () => {
+    navButton.classList.toggle('show');
+    navlinks.classList.toggle('show');
+});
