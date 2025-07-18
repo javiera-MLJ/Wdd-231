980fbe1b43ea512ddbaa90a96034aab702927ecd
javiera-MLJ <lor24003@byui.edu>
2025-07-18 14:04:25 -0400

    social media data

---
+++ scripts/members.js
@@ -0,0 +1,28 @@
+{
+    "new_members": {
+        "family_name": "Martinez",
+        "date": "09-12-23",
+        "number_of_people": 5,
+        "visit_of_bishopric": true
+    },
+
+    "individual_family_data": [
+        {
+        "name": "fabiola Martinez",
+        "gender": "female",
+        "birthdate": "1993-06-28"
+        },
+
+        {
+        "name": "Matias Martinez",
+        "gender": "male",
+        "birthdate": "1990-11-17"
+        },
+
+        {
+        "name": "Ricardo Martinez",
+        "gender": "male",
+        "birthdate": "2021-03-25"
+        }
+    ]
+}
